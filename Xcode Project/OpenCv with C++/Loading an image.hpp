18f120317c663c24ec3f9a68e0ab1a43efdd9f07
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-10-27 02:49:43 +0530

    xcode files

---
+++ Xcode Project/OpenCv with C++/Loading an image.hpp
@@ -0,0 +1,14 @@
+//
+//  Loading an image.hpp
+//  OpenCv with C++
+//
+//  Created by Nikhil Ramesh on 27/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#ifndef Loading_an_image_hpp
+#define Loading_an_image_hpp
+
+#include <stdio.h>
+
+#endif /* Loading_an_image_hpp */
