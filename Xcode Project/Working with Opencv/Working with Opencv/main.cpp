652330174ae9a9f03cc1da44572c74fab49525fd
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-10-27 03:08:55 +0530

    working code for c++ opencv in xcode project

---
+++ Xcode Project/Working with Opencv/Working with Opencv/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 27/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+    Mat img = imread("lena.png");
+    namedWindow("image", WINDOW_NORMAL);
+    imshow("image", img);
+    waitKey(0);
+    return 0;
+}
