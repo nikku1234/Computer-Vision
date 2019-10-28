b5b4be7fbda0e82f50c21c93616bc6e0913cd8b4
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-10-29 00:43:37 +0530

    webcam

---
+++ Xcode Project/Working with Opencv/Working with Opencv/working_with_web_cam.hpp
@@ -0,0 +1,14 @@
+//
+//  working_with_web_cam.hpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 29/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#ifndef working_with_web_cam_hpp
+#define working_with_web_cam_hpp
+
+#include <stdio.h>
+
+#endif /* working_with_web_cam_hpp */
