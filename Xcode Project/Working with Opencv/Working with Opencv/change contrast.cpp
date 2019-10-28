d4e2d19b2706c0158dc46071b09ff907e6a64039
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-10-29 00:59:09 +0530

    contrast

---
+++ Xcode Project/Working with Opencv/Working with Opencv/change contrast.cpp
@@ -0,0 +1,9 @@
+//
+//  change contrast.cpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 29/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#include "change contrast.hpp"
---
+++ Xcode Project/Working with Opencv/Working with Opencv/change contrast.hpp
@@ -0,0 +1,14 @@
+//
+//  change contrast.hpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 29/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#ifndef change_contrast_hpp
+#define change_contrast_hpp
+
+#include <stdio.h>
+
+#endif /* change_contrast_hpp */
