4318a8b25db84a9bd23582b9c41985811e95d3b4
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-10-30 14:08:30 +0530

    Gaussin Blur

---
+++ Xcode Project/Working with Opencv/Working with Opencv/Gaussian Blur.cpp
@@ -0,0 +1,58 @@
+//
+//  Gaussian Blur.cpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 30/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#include "Gaussian Blur.hpp"
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Read the image file
+    Mat image = imread("nik.jpg");
+
+    // Check for failure
+    if (image.empty())
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
+
+    //Blur the image with 3x3 Gaussian kernel
+    Mat image_blurred_with_3x3_kernel;
+    GaussianBlur(image, image_blurred_with_3x3_kernel, Size(3, 3), 0);
+
+    //Blur the image with 5x5 Gaussian kernel
+    Mat image_blurred_with_5x5_kernel;
+    GaussianBlur(image, image_blurred_with_5x5_kernel, Size(5, 5), 0);
+
+    //Define names of the windows
+    String window_name = "Nik";
+    String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
+    String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
+
+    // Create windows with above names
+    namedWindow(window_name);
+    namedWindow(window_name_blurred_with_3x3_kernel);
+    namedWindow(window_name_blurred_with_5x5_kernel);
+
+    // Show our images inside the created windows.
+    imshow(window_name, image);
+    imshow(window_name_blurred_with_3x3_kernel, image_blurred_with_3x3_kernel);
+    imshow(window_name_blurred_with_5x5_kernel, image_blurred_with_5x5_kernel);
+
+    waitKey(0); // Wait for any keystroke in the window
+
+    destroyAllWindows(); //destroy all opened windows
+
+    return 0;
+}
---
+++ Xcode Project/Working with Opencv/Working with Opencv/Gaussian Blur.hpp
@@ -0,0 +1,14 @@
+//
+//  Gaussian Blur.hpp
+//  Working with Opencv
+//
+//  Created by Nikhil Ramesh on 30/10/19.
+//  Copyright © 2019 Nikhil Ramesh. All rights reserved.
+//
+
+#ifndef Gaussian_Blur_hpp
+#define Gaussian_Blur_hpp
+
+#include <stdio.h>
+
+#endif /* Gaussian_Blur_hpp */
